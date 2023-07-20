bbb6ee9a0a4ba3acd62f6f9bbce28f0b7aebaaae
abdulazizboyqoziyev <118582265+abdulazizboyqoziyev@users.noreply.github.com>
2023-07-20 17:53:23 +0500

    Initial Commit

---
+++ QuizApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QuizApp
+//
+//  Created by Abdulaziz Boyqoziyev on 20/07/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
